95ef282631424af1c028382ae6c713d873ffbde2
root <rehan81981@gmail.com>
2024-01-09 12:07:45 +0000

    new repo

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# set the name of docker images and tag 
+IMAGE_NAME="reactjs-guvi"
+IMAGE_TAG="latest"
+
+
+# Build the Docker image
+docker-compose build
---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+#The image name and tag 
+IMAGE_NAME="reactjs-guvi"
+IMAGE_TAG="latest"
+
+
+#deploying the image to server
+docker-compose up -d
